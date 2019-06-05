c8879cae011da94b263cd0ea0955847eb4ea0ab9
CalieR <calielrushton@gmail.com>
2019-06-05 12:08:59 +0100

    security update

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -1,18 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import logo from './logo.svg';
-import './app.css';
-import { 
-  Route, 
-  Link, 
-  Switch,
-  Redirect 
-} from 'react-router-dom';
+import "./app.css";
+import { Route, Link, Switch, Redirect } from "react-router-dom";
 
-import Home from './../../pages/home';
-import Profile from './../../pages/profile';
-import Galleries from './../../pages/galleries';
-import Animals from './../../pages/animal-pictures';
-import Footer from './../../components/footer';
+import Home from "./../../pages/home";
+import Profile from "./../../pages/profile";
+import Galleries from "./../../pages/galleries";
+import Animals from "./../../pages/animal-pictures";
+import Footer from "./../../components/footer";
 
 class App extends Component {
   render() {
@@ -24,27 +19,40 @@ class App extends Component {
         </header>
         <div className="menu">
           <ul>
-            <li> <Link to="/">Home</Link> </li>
-            <li> <Link to="/galleries">Galleries</Link> </li>
-            <li> <Link to="/animal-pictures">Animals</Link> </li>
-            <li> <Link to="/profile">Profile</Link> </li> 
+            <li>
+              {" "}
+              <Link to="/">Home</Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/galleries">Galleries</Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/animal-pictures">Animals</Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/profile">Profile</Link>{" "}
+            </li>
           </ul>
         </div>
         <div className="App-intro">
           <Switch>
             {/* Routes with path props */}
-            <Route exact path="/" component={Home}/>
-            <Route path="/galleries" component={Galleries}/>
-            <Route path="/animal-pictures" component={Animals}/>
-            <Route path="/profile" component={Profile}/>
+            <Route exact path="/" component={Home} />
+            <Route path="/galleries" component={Galleries} />
+            <Route path="/animal-pictures" component={Animals} />
+            <Route path="/profile" component={Profile} />
             <Redirect to="/" />
           </Switch>
         </div>
-        <Footer></Footer> 
+        <Footer className="App-footer" person="Chavers" />
+        <Footer className="App-footer" person="Bob" />
+        <Footer className="App-footer" person="Jalalabab" />
       </div>
     );
   }
-};
-
+}
 
 export default App;
